442bd1ad53109b8c4915c4266afbc2835e748f4b
krasteki <kiril.krastev@hashicorp.com>
2022-03-31 09:19:29 +0300

    first_commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.1.0"
+
+      #configuration_aliases = [ aws.alternate ]
+    }
+  }
+
+  required_version = "~>1.1.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "Ireland"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+  alias  = "Paris"
+}
